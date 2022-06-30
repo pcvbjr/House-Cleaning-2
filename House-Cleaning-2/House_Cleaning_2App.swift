1931e200e3331d26cf07ad74c8f31863e5c138b8
pcvbjr <pcvbjr@gmail.com>
2022-06-29 22:05:38 -0400

    Initial Commit

---
+++ House-Cleaning-2/House_Cleaning_2App.swift
@@ -0,0 +1,17 @@
+//
+//  House_Cleaning_2App.swift
+//  House-Cleaning-2
+//
+//  Created by Chris Van Buren on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct House_Cleaning_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ House-Cleaning-2Tests/House_Cleaning_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  House_Cleaning_2Tests.swift
+//  House-Cleaning-2Tests
+//
+//  Created by Chris Van Buren on 6/29/22.
+//
+
+import XCTest
+@testable import House_Cleaning_2
+
+class House_Cleaning_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
